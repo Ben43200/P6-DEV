fa672f413ece39a62bafff2b41c28f66e52b451d
Ben <meteoben@gmail.com>
2022-06-13 15:02:51 +0200

    last commit lionel create sauce ok

--- controllers/sauceCtrls.js
+++ controllers/sauceCtrls.js
@@ -2,21 +2,34 @@
 const fs = require('fs');
 // const sauce = require('../models/ModelsSauce');
 
-const Sauce = require('../models/ModelsSauce');
+const Sauce = require('../models/Sauce');
 
-exports.createSauce = (req, res, next) => {
-  const sauceObject = JSON.parse(req.body.sauce);
-  delete sauceObject._id;
-  const sauce = new Sauce({
-    ...sauceObject,
-    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-  });
-  console.log(sauce);
-  sauce.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-    .catch(error => res.status(400).json({ error }));
+exports.createSauce = async (req, res, next) => {
+  try {
+    const sauceObject = JSON.parse(req.body.sauce);
+    delete sauceObject._id;
+    const sauce = new Sauce({
+      ...sauceObject,
+      userId: req.authentifiedUserId,
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${
+        req.file.filename
+      }`,
+      likes: 0,
+      dislikes: 0,
+      usersLiked: [],
+      usersDisliked: [],
+      // imageUrl: `${req.protocol}://${req.get('host')}/images/${
+      //   req.file.filename
+      // }`,
+      // userId: req.auth.userId,
+    });
+    // console.log(sauce);
+    await sauce.save();
+    res.status(201).json({ message: 'Objet enregistré !' });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
-
 
 exports.getOneSauce = async (req, res, next) => {
   try {
@@ -32,30 +45,34 @@ exports.getOneSauce = async (req, res, next) => {
 };
 
 exports.modifySauce = (req, res, next) => {
-  const sauceObject = req.file ?
-    {
-      ...JSON.parse(req.body.sauce),
-      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    } : { ...req.body };
-  Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-    .catch(error => res.status(400).json({ error }));
+  const sauceObject = req.file
+    ? {
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${
+          req.file.filename
+        }`,
+      }
+    : { ...req.body };
+  Sauce.updateOne(
+    { _id: req.params.id },
+    { ...sauceObject, _id: req.params.id }
+  )
+    .then(() => res.status(200).json({ message: 'Objet modifié !' }))
+    .catch((error) => res.status(400).json({ error }));
 };
 
-
-  exports.deleteSauce = (req, res, next) => {
-    Sauce.findOne({ _id: req.params.id })
-      .then(Sauce => {
-        const filename = Sauce.imageUrl.split('/images/')[1];
-        fs.unlink(`images/${filename}`, () => {
-          Sauce.deleteOne({ _id: req.params.id })
-            .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-            .catch(error => res.status(400).json({ error }));
-        });
-      })
-      .catch(error => res.status(500).json({ error }));
-  };
-   
+exports.deleteSauce = (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id })
+    .then((Sauce) => {
+      const filename = Sauce.imageUrl.split('/images/')[1];
+      fs.unlink(`images/${filename}`, () => {
+        Sauce.deleteOne({ _id: req.params.id })
+          .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
+          .catch((error) => res.status(400).json({ error }));
+      });
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
 
 exports.getAllSauce = (req, res, next) => {
   Sauce.find()
--- models/Sauce.js
+++ models/Sauce.js
@@ -12,7 +12,6 @@ const  sauceSchema = new mongoose.Schema({
     name: { type: String, required: true },
     manufacturer: { type: String, required: true },
     description: { type: String, required: true },
-    title: { type: String, required: true },
     mainPepper: { type: String, required: true },
     imageUrl: { type: String, required: true },
     heat: { type: Number, required: true },

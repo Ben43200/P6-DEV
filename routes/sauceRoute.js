91887d6be45de782e51d0f3d4de5250586717a66
Ben <meteoben@gmail.com>
2022-07-04 09:51:08 +0200

    last commit avec la logique des likes

--- routes/sauceRoute.js
+++ routes/sauceRoute.js
@@ -13,6 +13,8 @@ router.post('/', auth, multer,  sauceCtrl.createSauce);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.post('/:id/like', auth, sauceCtrl.likeDislike)
+
 
 module.exports = router;
 
